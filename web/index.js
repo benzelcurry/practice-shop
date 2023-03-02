c6cd5ef47e71c2c24644ab0ff31352d21f38a31a
benzelcurry <benawhiting@gmail.com>
2023-03-02 14:15:14 -0800

    Link back end to front end

--- web/index.js
+++ web/index.js
@@ -6,6 +6,7 @@ import serveStatic from "serve-static";
 
 import shopify from "./shopify.js";
 import applyQrCodeApiEndpoints from "./middleware/qr-code-api.js";
+import applyQrCodePublicEndpoints from "./middleware/qr-code-public.js";
 import GDPRWebhookHandlers from "./gdpr.js";
 
 const PORT = parseInt(process.env.BACKEND_PORT || process.env.PORT, 10);
@@ -32,6 +33,7 @@ app.post(
 // If you are adding routes outside of the /api path, remember to
 // also add a proxy rule for them in web/frontend/vite.config.js
 
+applyQrCodePublicEndpoints(app);
 app.use("/api/*", shopify.validateAuthenticatedSession());
 
 app.use(express.json());
